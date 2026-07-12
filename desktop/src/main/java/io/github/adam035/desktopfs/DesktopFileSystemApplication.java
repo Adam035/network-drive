03008a6d37162ca191a34c928d510061b066d3c2
adam035 <adamlipian2723@gmail.com>
2026-07-13 00:30:15 +0200

    chore: set up WinFsp desktop application

---
+++ desktop/src/main/java/io/github/adam035/desktopfs/DesktopFileSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.adam035.desktopfs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesktopFileSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesktopFileSystemApplication.class, args);
+	}
+
+}
