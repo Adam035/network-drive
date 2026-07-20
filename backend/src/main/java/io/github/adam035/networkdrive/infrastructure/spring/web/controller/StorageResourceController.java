eee1cc17874a922141196f81e1607a54acf38ec7
adam035 <adamlipian2723@gmail.com>
2026-07-21 00:10:51 +0200

    fix: add wildcard controller methods

--- backend/src/main/java/io/github/adam035/networkdrive/infrastructure/spring/web/controller/StorageResourceController.java
+++ backend/src/main/java/io/github/adam035/networkdrive/infrastructure/spring/web/controller/StorageResourceController.java
@@ -3,6 +3,7 @@ package io.github.adam035.networkdrive.infrastructure.spring.web.controller;
 import io.github.adam035.networkdrive.application.usecase.DeleteStorageResourceUseCase;
 import io.github.adam035.networkdrive.application.usecase.GetStorageResourceUseCase;
 import io.github.adam035.networkdrive.domain.model.StorageResource;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +17,16 @@ public class StorageResourceController {
 
     private final DeleteStorageResourceUseCase deleteStorageResourceUseCase;
 
-    @GetMapping
-    public StorageResource getStorageResource(@RequestParam String path) {
+    @GetMapping("/**")
+    public StorageResource getStorageResource(HttpServletRequest request) {
+        String path = request.getRequestURI().replace("/storage-resources", "");
         return getStorageResourceUseCase.getStorageResource(path);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/**")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteStorageResource(@RequestParam String path) {
+    public void deleteStorageResource(HttpServletRequest request) {
+        String path = request.getRequestURI().replace("/storage-resources", "");
         deleteStorageResourceUseCase.deleteStorageResource(path);
     }
 
